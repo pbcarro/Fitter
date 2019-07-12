739a61afa700c415da74d78e5190b36a76632fd5
pbcarro <pbcarro@gmail.com>
2019-07-12 00:23:45 -0400

    Add files via upload

---
+++ DatFileBuilder.py
@@ -0,0 +1,37 @@
+'''
+Program for merging individual eigenvalue files into a full file
+-Currently it's own program so I can build catalogs only as large as I want to
+'''
+
+import numpy as np
+
+JStart = 0		#Starting J
+JStop = 25		#Ending J
+delta = 1.0E-2	#Resolution of the eigenvalue file
+Delta = np.arange(-1.0,1.0+delta,delta)	
+StateCount = 0
+for i in range(JStart,JStop):	#Count up how many states there are in total
+	StateCount += i*2+1
+
+DatTable = np.zeros((StateCount,len(Delta)))	#Build a matrix to hold all the data
+Count = 0
+
+for i in range(JStart,JStop+1):		#As with the other programs this is to get the last J value instead of JStop-1
+	CurrentState = np.loadtxt("EigenVals/%d_dk2.dat" % i)
+	if (i == 0):		#The J=0 eigenvalue table is a 1D array, not 2D, so it needs slightly different code
+		DatTable[Count] = CurrentState
+		Count +=1
+	else:				#For J>0 we pull the values iteratively
+		for j in range(len(CurrentState)):
+			DatTable[Count] = CurrentState[j]
+			Count +=1
+
+#Write the merged values to a file
+FileStr = ""
+for i in range(len(DatTable)):
+	for j in range(len(Delta)):
+		FileStr += "%.16f\t" % DatTable[i][j]
+	FileStr += "\n"
+FileHandle = open ("J%d_%d_dk2.dat" % (JStart,JStop),'w')
+FileHandle.write(FileStr)
+FileHandle.close()
