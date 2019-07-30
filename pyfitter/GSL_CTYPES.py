b3f4ecccf89d78e73b1189259fcdbf41990742c8
Brandon Carroll <pbcarro@gmail.com>
2019-07-30 12:54:34 -0400

    Update GSL_CTYPES.py
    Finally Fixed

--- pyfitter/GSL_CTYPES.py
+++ pyfitter/GSL_CTYPES.py
@@ -1,9 +1,4 @@
-import numpy as np
-from ctypes import *
-
-
-
-#### GSL structs
+from ctypes import c_uint, c_int, c_double, create_string_buffer, CDLL, POINTER, byref, Structure, c_size_t, CFUNCTYPE, c_void_p, c_char
 
 ###Level 0 structures
 class GSL_block(Structure):
@@ -36,19 +31,19 @@ class GSL_matrix(Structure):
 free_type = CFUNCTYPE(	c_void_p,
 						c_void_p
 					)
-
+#Looks Correct	
 f_type = CFUNCTYPE(	c_int,
 					POINTER(GSL_vector),
 					c_void_p,
 					POINTER(GSL_vector)
 					)
-
+#Looks Correct	
 df_type = CFUNCTYPE(c_int,
 					POINTER(GSL_vector),
 					c_void_p,
 					POINTER(GSL_matrix)
 					)
-					
+#Looks Correct					
 fvv_type = CFUNCTYPE(	c_int, 
 						POINTER(GSL_vector), 
 						POINTER(GSL_vector), 
@@ -56,67 +51,77 @@ fvv_type = CFUNCTYPE(	c_int,
 						POINTER(GSL_vector)
 					)
 
+#Looks Correct	
 trs_alloc_type = CFUNCTYPE	(	c_void_p,
 							c_void_p,
 							c_size_t,
 							c_size_t
 						)
-
+#Looks Correct	
 trs_init_type = CFUNCTYPE(	c_int,
 							c_void_p,
 							c_void_p
 						)
-
+#Looks Correct	
 preloop_type = CFUNCTYPE(	c_int,
 							c_void_p,
 							c_void_p
 						)
-
+#Looks Correct	
 step_type = CFUNCTYPE	(	c_int,
-							c_void_p,
-							c_double,POINTER(GSL_vector),
-							c_void_p
+							c_void_p,				#vtrust_state
+							c_double,				#delta
+							POINTER(GSL_vector),	#dx
+							c_void_p				#vstate
 						)
-
-preduction_type = CFUNCTYPE(c_int,
-							c_void_p,
-							POINTER(GSL_vector),
-							c_double,
-							c_void_p
+#Looks Correct	
+preduction_type = CFUNCTYPE(c_int,	
+							c_void_p,				#vtrust_state
+							POINTER(GSL_vector),	#dx
+							c_double,				#pred
+							c_void_p				#vstate
 							)
-
-scale_init_type = CFUNCTYPE(c_int,
+#Looks Correct	
+scale_init_type = CFUNCTYPE(c_int,		#
 							POINTER(GSL_matrix),
 							POINTER(GSL_vector)
 							)
-
+#Looks Correct	
 scale_update_type = CFUNCTYPE(	c_int,
 								POINTER(GSL_matrix),
 								POINTER(GSL_vector)
 							)
-
-solver_alloc_type = CFUNCTYPE	(	c_void_p,
-							c_void_p,
-							c_size_t,
-							c_size_t
-						)
-
-solver_init_type = CFUNCTYPE(	c_int,
-								c_void_p,
-								c_void_p
+#Looks Correct	
+solver_alloc_type = CFUNCTYPE	(	c_void_p,	#Return
+									c_size_t,	#n
+									c_size_t	#p
+								)
+#Looks Correct	
+solver_init_type = CFUNCTYPE(	c_int,			#Return
+								c_void_p,		#vtrust_state
+								c_void_p		#vstate
 							)
-
-solver_presolve_type = CFUNCTYPE(	c_int,
-									c_double,
-									c_void_p,
-									c_void_p
+#Looks Correct	
+solver_presolve_type = CFUNCTYPE(	c_int,		#Return
+									c_double,	#mu
+									c_void_p,	#vtrust_state
+									c_void_p	#vstate
 								)
 
-solver_rcond_type = CFUNCTYPE(	c_int,
-								c_double,
-								c_void_p
-							)
+#Looks Correct	
+solver_solve_type = CFUNCTYPE	(	c_int,				#Return
+									POINTER(GSL_vector),#f
+									POINTER(GSL_vector),#x
+									c_void_p,	#vtrust_state
+									c_void_p	#vstate
+								)
 
+#Looks Correct	
+solver_rcond_type = CFUNCTYPE(	c_int,				#Return
+								POINTER(c_double),	#rcond
+								c_void_p			#vstate
+							)
+#Looks Correct	
 class GSL_fdf(Structure):
 	_fields_=	[	
 		("f",		f_type),
@@ -129,7 +134,7 @@ class GSL_fdf(Structure):
 		("nevaldf",	c_size_t),
 		("nevalfvv",c_size_t)
  		]
-
+#Looks Correct	
 class GSL_nlinear_trs(Structure):
 	_fields_=	[	
 		("name",		POINTER(c_char)),
@@ -140,20 +145,21 @@ class GSL_nlinear_trs(Structure):
 		("preduction",	preduction_type),
 		("free",		free_type)
  		]
-
+#Looks Correct	
 class GSL_nlinear_scale(Structure):
 	_fields_=	[	
 		("name",	POINTER(c_char)),
 		("init",	scale_init_type),
 		("update",	scale_update_type)
  		]
-
+#Looks Correct	
 class GSL_nlinear_solver(Structure):
 	_fields_=	[	
 		("name",	POINTER(c_char)),
 		("alloc",	solver_alloc_type),
 		("init",	solver_init_type),
 		("presolve",solver_presolve_type),
+		("solve", 	solver_solve_type),
 		("rcond",	solver_rcond_type),
 		("free",	free_type),
  		]
@@ -173,42 +179,43 @@ class GSL_multifit_nlinear_parameters(Structure):
  		]
 
 ###Function pointer types for Level 2 gsl structures
-nlinear_alloc_type = CFUNCTYPE	(	c_void_p,
-									POINTER(GSL_multifit_nlinear_parameters),
-									c_size_t,
-									c_size_t
+#Looks Correct	
+nlinear_alloc_type = CFUNCTYPE	(	c_void_p,	#Return
+									POINTER(GSL_multifit_nlinear_parameters),	#params
+									c_size_t,	#n
+									c_size_t	#p
 								)
-
-nlinear_init_type = CFUNCTYPE	(	c_int,
-									c_void_p,
-									POINTER(GSL_vector),
-									POINTER(GSL_fdf),
-									POINTER(GSL_vector),
-									POINTER(GSL_vector),
-									POINTER(GSL_matrix),
-									POINTER(GSL_vector)
+#Looks Correct	
+nlinear_init_type = CFUNCTYPE	(	c_int,		#Return
+									c_void_p,	#state
+									POINTER(GSL_vector),	#wts
+									POINTER(GSL_fdf),		#fdf
+									POINTER(GSL_vector),	#x
+									POINTER(GSL_vector),	#f
+									POINTER(GSL_matrix),	#J
+									POINTER(GSL_vector)		#g
 								)
-
-nlinear_iterate_type = CFUNCTYPE(	c_int,
-									c_void_p,
-									POINTER(GSL_vector),
-									POINTER(GSL_fdf),
-									POINTER(GSL_vector),
-									POINTER(GSL_vector),
-									POINTER(GSL_matrix),
-									POINTER(GSL_vector),
-									POINTER(GSL_vector)
+#Looks Correct	
+nlinear_iterate_type = CFUNCTYPE(	c_int,					#Return
+									c_void_p,				#state
+									POINTER(GSL_vector),	#wts
+									POINTER(GSL_fdf),		#fdf
+									POINTER(GSL_vector),	#x
+									POINTER(GSL_vector),	#f
+									POINTER(GSL_matrix),	#J
+									POINTER(GSL_vector),	#g
+									POINTER(GSL_vector)		#dx
 								)
-
-nlinear_type_rcond_type = CFUNCTYPE(c_int,
-									c_double,
-									c_void_p
+#Looks Correct	
+nlinear_type_rcond_type = CFUNCTYPE(c_int,				#Return
+									POINTER(c_double),	#rcond
+									c_void_p			#state
 									)
-
-nlinear_avratio_type = CFUNCTYPE(	c_double,
-									c_void_p
+#Looks Correct	
+nlinear_avratio_type = CFUNCTYPE(	c_double,		#Return
+									c_void_p		#state
 								)
-
+#Looks Correct	
 class GSL_multifit_nlinear_type(Structure):
 	_fields_=	[	
 		("name",	POINTER(c_char)),
@@ -220,11 +227,11 @@ class GSL_multifit_nlinear_type(Structure):
 		("free",	free_type)
  		]
 
-
+#Looks Correct	
 class GSL_multifit_nlinear_workspace(Structure):
 	_fields_=	[	
 		("type",			POINTER(GSL_multifit_nlinear_type)),
-		("fdf",				GSL_fdf),
+		("fdf",				POINTER(GSL_fdf)),
 		("x",				POINTER(GSL_vector)),
 		("f",				POINTER(GSL_vector)),
 		("dx",				POINTER(GSL_vector)),
@@ -236,14 +243,3 @@ class GSL_multifit_nlinear_workspace(Structure):
 		("params",			GSL_multifit_nlinear_parameters),
 		("state",			c_void_p)
  		]	
-
-
-
-
-
-
-
-
-
-
-
