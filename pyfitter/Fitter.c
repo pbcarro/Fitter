4e0e17112bb6ce0a6029e99aa92e1b3103efdfbd
pbcarro <pbcarro@gmail.com>
2022-06-10 12:37:46 -0400

    Get_Catalog V2/Get_Catalog with DJ/More Robust Tests
    Added Get_Catalog V2 much faster than the original and should replace it
    Added DJ correction functions, more need to be done but it's a start
    More robust timing and accuracy tests
    Added more PyExample code and cleaned it up some

--- pyfitter/Fitter.c
+++ pyfitter/Fitter.c
@@ -5,6 +5,7 @@ Generic version of the fitting program
 
 Build Command:
 gcc -Wall -o Go Fitter.c -lm -lgsl -lgslcblas -O3 -funroll-loops
+gcc -Wall -o Go Fitter.c -lm -O3 -funroll-loops
 
 Ctypes Shared Lib:
 gcc -Wall -o Fitter.so -shared -fPIC -O3 -funroll-loops Fitter.c -lm -lgsl -lgslcblas
@@ -20,15 +21,22 @@ gcc -Wall -o Fitter.so -shared -fPIC -O3 -funroll-loops Fitter.c -lm -lgsl -lgsl
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_multifit_nlinear.h>
 #include <gsl/gsl_linalg.h>
+// #include "GSL_Include/vector/gsl_vector.h"
+// #include "GSL_Include/blas/gsl_blas.h"
+// #include "GSL_Include/matrix/gsl_matrix.h"
+// #include "GSL_Include/multifit_nlinear/gsl_multifit_nlinear.h"
+// #include "GSL_Include/linalg/gsl_linalg.h"
 #include "Fitter.h"
 
 
 //Functions
 int main (int argc, char *argv[])
 {
-	Test_SBFIT();
-	Timing_Test();
-	Accuracy_Test();	
+	//Test_SBFIT();
+	//Timing_Test();
+	Accuracy_Test();
+	//Timing_Test_Triples();
+		
 	return 1;
 }
 
